674ce62e589cced61f33ade47e5d395fa636510e
kolesnikov <kolesnikov>
2022-12-16 11:40:51 +0300

    add child

--- lab1.12/member.h
+++ lab1.12/member.h
@@ -4,7 +4,22 @@ using namespace std;
 class date
 {
 	int d; int m; int y;
+public:
+	void edit()
+	{
 
+	}
+	int gety()
+	{
+		return y;
+	}
+
+	int operator-(date* obj)
+	{
+		return (y - obj->gety());
+	}
+
+	// cout << overload
 };
 
 class member
@@ -64,30 +79,47 @@ public:
 
 	void addchild()
 	{
+		int originalSize = children_size;
+		children_size++;
+		member* new_child = new member;
+		member** created = new member * [children_size];
 
+		for (int i = 0; i < originalSize; i++)
+		{
+			created[i] = new member;
+			created[i] = children[i];
+		}
+
+		children = created;
+		children[children_size - 1] = new_child;
 	}
+
 	void delchild()
 	{
 
 	}
 	void editchild()
 	{
+		// get i
 
 	}
 
 	void editbirth()
 	{
-
+		birth->edit();
 	}
 	void editdeath()
 	{
-
+		death->edit();
 	}
-	void editage()
+
+	void setage()
 	{
-
+		if (death->gety() != -1)
+			age = *death - birth;
+		else
+			age = 2022 - birth->gety();
 	}
-	
 
 
 
